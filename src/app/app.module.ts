19620739e42c1c94a6a7bca3e20097fe77dfe674
Bennet Witczak <bennet.witczak@icloud.com>
2023-11-07 19:33:48 +0100

    Implement Lets Work together (About Me)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadComponent } from './head/head.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadComponent
+    HeadComponent,
+    AboutmeComponent
   ],
   imports: [
     BrowserModule,
