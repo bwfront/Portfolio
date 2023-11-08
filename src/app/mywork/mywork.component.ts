3e6f8c91e2f5a63f287d715870c37da332642244
Bennet Witczak <bennet.witczak@icloud.com>
2023-11-08 18:21:26 +0100

    My Work HTML CSS Desktop Finish

--- src/app/mywork/mywork.component.ts
+++ src/app/mywork/mywork.component.ts
@@ -23,5 +23,13 @@ export class MyworkComponent {
         'Navigate the underwater world as Sharkie. Overcome enemies, avoid dangers, and collect coins in a straightforward aquatic adventure. Face the endboss in a simple battle of wit and reflexes.',
       github: 'https://github.com/bwfront/Sharkie',
     },
+    {
+      title: 'Sharkie',
+      img: 'sharkie.png',
+      devLanguage: 'JavaScript | HTML | CSS',
+      description:
+        'Navigate the underwater world as Sharkie. Overcome enemies, avoid dangers, and collect coins in a straightforward aquatic adventure. Face the endboss in a simple battle of wit and reflexes.',
+      github: 'https://github.com/bwfront/Sharkie',
+    },
   ];
 }
