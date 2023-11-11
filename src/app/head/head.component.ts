ce8aa1699664d510fb86c4894022ac12495b480f
Bennet Witczak <bennet.witczak@icloud.com>
2023-11-11 21:36:40 +0100

    Implement Burger Menu

--- src/app/head/head.component.ts
+++ src/app/head/head.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./head.component.sass']
 })
 export class HeadComponent {
-
+  mobilemenu: boolean = false;
+  mobileMenu(){
+    this.mobilemenu = !this.mobilemenu;
+  }
 }
